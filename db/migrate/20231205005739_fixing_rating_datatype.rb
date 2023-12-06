77356267dfb69f251a6cc38a8997dd157741ff80
Evan Kandell <ekandell@berkeley.edu>
2023-12-06 09:56:59 +0000

    migrations problem

--- db/migrate/20231205005739_fixing_rating_datatype.rb
+++ db/migrate/20231205005739_fixing_rating_datatype.rb
@@ -2,7 +2,7 @@
 
 class FixingRatingDatatype < ActiveRecord::Migration[5.2]
   def change
-    remove_column :news_items, :rating, :integer
+    remove_column :news_items, :rating
     add_column :news_items, :rating, :integer
   end
 end
