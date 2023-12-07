4bf979a2daae59d0d877837892ad797c72a1d91b
Xiaowen Liu <xiaowen_liu@berkeley.edu>
2023-12-07 00:09:27 +0000

    please

--- db/migrate/20231205005739_fixing_rating_datatype.rb
+++ db/migrate/20231205005739_fixing_rating_datatype.rb
@@ -2,7 +2,7 @@
 
 class FixingRatingDatatype < ActiveRecord::Migration[5.2]
   def change
-    remove_column :news_items, :rating
+    remove_column :news_items, :rating, :integer
     add_column :news_items, :rating, :integer
   end
 end
